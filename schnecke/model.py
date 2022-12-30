8818f5e25cff0a58cf04a61267bb2570a5d6728f
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-12-30 15:43:23 -0500

    cumsum the e2s and e4s

--- schnecke/model.py
+++ schnecke/model.py
@@ -7,11 +7,12 @@ from jax_cosmo.scipy.interpolate import InterpolatedUnivariateSpline
 import jaxopt
 
 from .jax_helpers import ln_gmm_density
+# from .pchip import pchip_interpolate_uniform
 
 
 @jax.jit
 def distort_rz(rz, th, e2, e4):
-    return 1 - e2 * rz * jnp.cos(2 * th) - e4 * rz * jnp.cos(4 * th)
+    return 1 + e2 * rz * jnp.cos(2 * th) + e4 * rz * jnp.cos(4 * th)
 
 
 class VerticalOrbitModel:
@@ -21,34 +22,48 @@ class VerticalOrbitModel:
         self.e4_knots = jnp.array(e4_knots)
 
     @partial(jax.jit, static_argnames=['self'])
-    def get_distorted_rz(self, init_rz, init_th, e2_vals, ln_e4_vals):
-        # TODO: figure out also how to get out the corrected thetas in here?
-        e2_interp = InterpolatedUnivariateSpline(
+    def get_e2_e4(self, init_rz, e2_vals, e4_vals):
+        # e2s = pchip_interpolate_uniform(
+        #     self.e2_knots,
+        #     e2_vals,
+        #     init_rz
+        # )
+        # e4s = pchip_interpolate_uniform(
+        #     self.e4_knots,
+        #     e4_vals,
+        #     init_rz
+        # )
+        e2s = InterpolatedUnivariateSpline(
             self.e2_knots,
-            e2_vals,
-            k=3
-        )
-        ln_e4_interp = InterpolatedUnivariateSpline(
+            jnp.cumsum(e2_vals),
+            k=1
+        )(init_rz)
+        e4s = InterpolatedUnivariateSpline(
             self.e4_knots,
-            ln_e4_vals,
-            k=3
-        )
+            jnp.cumsum(e4_vals),
+            k=1
+        )(init_rz)
+        return e2s, e4s
+
+    @partial(jax.jit, static_argnames=['self'])
+    def get_distorted_rz(self, init_rz, init_th, e2_vals, e4_vals):
+        e2s, e4s = self.get_e2_e4(init_rz, e2_vals, e4_vals)
         rz = init_rz * distort_rz(
-            e2=e2_interp(init_rz),
-            e4=jnp.exp(ln_e4_interp(init_rz)),
+            e2=e2s,
+            e4=e4s,
             rz=init_rz,
             th=init_th
         )
         return rz
 
     @partial(jax.jit, static_argnames=['self'])
-    def get_rz_th(self, z, vz, Omega, e2_vals, ln_e4_vals):
+    def get_rz_th(self, z, vz, Omega, e2_vals, e4_vals):
         x = vz / jnp.sqrt(Omega)
         y = z * jnp.sqrt(Omega)
 
         init_rz = jnp.sqrt(x**2 + y**2)
         init_th = jnp.arctan2(y, x)
-        rz = self.get_distorted_rz(init_rz, init_th, e2_vals, ln_e4_vals)
+        rz = self.get_distorted_rz(init_rz, init_th, e2_vals, e4_vals)
 
         return rz, init_rz, init_th
 
@@ -58,7 +73,7 @@ class VerticalOrbitModel:
             z - params['z0'], vz - params['vz0'],
             Omega=jnp.exp(params['ln_Omega']),
             e2_vals=params['e2_vals'],
-            ln_e4_vals=params['ln_e4_vals']
+            e4_vals=params['e4_vals']
         )
 
         amps = jnp.exp(params['ln_amps'])
@@ -87,8 +102,7 @@ class VerticalOrbitModel:
             jaxopt_kwargs.setdefault('method', 'L-BFGS-B')
             optimizer = jaxopt.ScipyBoundedMinimize(
                 fun=self.objective,
-                **jaxopt_kwargs
-                # options=dict(disp=False)
+                **jaxopt_kwargs,
             )
             res = optimizer.run(
                 init_params=params0,
