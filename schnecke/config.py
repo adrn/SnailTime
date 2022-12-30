7294a1ebd464f3af052a7d6f441a8e8e40b72c48
Adrian Price-Whelan <adrian.prw@gmail.com>
2022-12-30 16:52:50 -0500

    set galactocentric params and update ignores

--- schnecke/config.py
+++ schnecke/config.py
@@ -1,5 +1,8 @@
 import astropy.coordinates as coord
+import astropy.units as u
 
+# Same values from Hunt+2022
 galcen_frame = coord.Galactocentric(
-    # TODO: update values
+    galcen_distance=8.275*u.kpc,
+    galcen_v_sun=[8.4, 251.8, 8.4] * u.km/u.s
 )
