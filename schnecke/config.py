f52c7ab81db042c381214fc505a17b387946ee9f
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-01-07 17:48:44 -0500

    add unit system

--- schnecke/config.py
+++ schnecke/config.py
@@ -1,8 +1,11 @@
 import astropy.coordinates as coord
 import astropy.units as u
+from gala.units import galactic
 
 # Same values from Hunt+2022
 galcen_frame = coord.Galactocentric(
-    galcen_distance=8.275*u.kpc,
+    galcen_distance=8.275 * u.kpc,
     galcen_v_sun=[8.4, 251.8, 8.4] * u.km/u.s
 )
+
+usys = galactic
