d07ca31854526147332f73da7627651bf37fc4d8
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-01-08 16:48:38 -0500

    remove height over plane

--- schnecke/config.py
+++ schnecke/config.py
@@ -8,7 +8,8 @@ from gala.units import galactic
 # Same values from Hunt+2022
 galcen_frame = coord.Galactocentric(
     galcen_distance=8.275 * u.kpc,
-    galcen_v_sun=[8.4, 251.8, 8.4] * u.km/u.s
+    galcen_v_sun=[8.4, 251.8, 8.4] * u.km/u.s,
+    z_sun=0*u.pc
 )
 
 usys = galactic
